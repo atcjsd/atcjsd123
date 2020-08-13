f7469187a87d4d6970fd84578c6564b4be5770f8
atcjsd <hyensu5538@gmail.com>
2020-08-13 13:13:01 +0900

    asd123

---
+++ articlewrite/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class articlebuy(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class articlefree(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class articlesale(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
